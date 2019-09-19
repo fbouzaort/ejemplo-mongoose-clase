9cbb62fe293c323f10acd158abd2921f34091806
Francisco Bouza <franciscobouza@Franciscos-MacBook-Pro.local>
2019-09-19 20:24:47 -0300

    env port

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ var userRouter = require("./routes/user");
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-     "PUT, POST, GET, DELETE, OPTIONS");
-     next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
 });
 
-app.use('/api', userRouter);
+app.use("/api", userRouter);
 
-mongoose.connect(config.mongo_url, { useNewUrlParser: true })
-.then(()=>{
+mongoose
+  .connect(config.mongo_url, { useNewUrlParser: true })
+  .then(() => {
     console.log("MongoDB successfully connected!");
 
-    app.listen(config.port, () => console.log(
-        "Application is running under port " + config.port));
-
-}).catch((err) => {
+    app.listen(process.env.PORT || config.port, () =>
+      console.log("Application is running under port " + config.port)
+    );
+  })
+  .catch(err => {
     console.log("Error connecting with MongoDB :-(");
-})
-
-
-
+  });
